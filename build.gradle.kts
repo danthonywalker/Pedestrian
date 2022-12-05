d65bc7d20165cc70e3cb845dedd48a0f7a59540e
NovaFox161 <novamaday@gmail.com>
2022-12-04 19:51:02 -0600

    Use GH Actions for CI/CD + setup docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     java
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
+
+    id("com.google.cloud.tools.jib") version "3.3.1"
 }
 
 group = "dev.neontech"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_HIGHER
+java.sourceCompatibility = JavaVersion.VERSION_19
 
 repositories {
     mavenCentral()
@@ -18,6 +20,14 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+jib {
+    var imageVersion = version.toString()
+    if (imageVersion.contains("SNAPSHOT")) imageVersion = "latest"
+
+    to.image = "rg.nl-ams.scw.cloud/dreamexposure/pedestrian:$imageVersion"
+    from.image = "eclipse-temurin:19-jre-alpine"
 }
 
 tasks.withType<Test> {
