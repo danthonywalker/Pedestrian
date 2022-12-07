9169f0308e4e3922144073d24d4f12b237a033fa
danthonywalker <thedanthonywalker@gmail.com>
2022-12-06 22:54:28 -0600

    Include Jackson Annotations for Immutables Objects

--- src/main/java/pedestrian/discord/interaction/model/Interaction.java
+++ src/main/java/pedestrian/discord/interaction/model/Interaction.java
@@ -1,7 +1,11 @@
 package pedestrian.discord.interaction.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
+@JsonDeserialize(as = ImmutableInteraction.class)
+@JsonSerialize(as = ImmutableInteraction.class)
 @Value.Immutable
 public interface Interaction {
 
--- src/main/java/pedestrian/discord/interaction/model/InteractionResponse.java
+++ src/main/java/pedestrian/discord/interaction/model/InteractionResponse.java
@@ -1,7 +1,11 @@
 package pedestrian.discord.interaction.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
+@JsonDeserialize(as = ImmutableInteractionResponse.class)
+@JsonSerialize(as = ImmutableInteractionResponse.class)
 @Value.Immutable
 public interface InteractionResponse {
 
